f793ede3c4a7610779477fbb5d9144dad118ccad
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-29 17:19:37 -0500

    build initial cars_controller#index action; finish CardSerializer#to_serialized_json

--- backend/flashcard-api/app/services/card_serializer.rb
+++ backend/flashcard-api/app/services/card_serializer.rb
@@ -1,9 +1,14 @@
 class CardSerializer
 
-    def intialize(card_object)
+    def initialize(card_object)
         @card_object = card_object
     end
 
     def to_serialized_json
+        options = {
+            except: [:created_at, :updated_at]
+        }
+
+        @card_object.to_json(options)
     end
 end
