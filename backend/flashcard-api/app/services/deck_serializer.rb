cd88701ec3c1ed1bcf08adf2a57df5f15cca8aca
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-29 16:43:08 -0500

    ibuild initial deck serializer

--- backend/flashcard-api/app/services/deck_serializer.rb
+++ backend/flashcard-api/app/services/deck_serializer.rb
@@ -1,2 +1,14 @@
 class DeckSerializer
+
+    def initialize(deck_object)
+        @deck_object = deck_object
+    end
+
+    def to_serialized_json
+        options = {
+            include: [:name, :language]
+        }
+
+        @deck_object.to_json(options)
+    end
 end
