10f46914cfce5092a4fa2aca7a25b6de5e161d3d
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-29 20:24:03 -0500

    build our initial ADD_DECKS and LOADING_DECKS cases in decksReducer

--- frontend/flashcards/src/reducers/decksReducer.js
+++ frontend/flashcards/src/reducers/decksReducer.js
@@ -3,6 +3,19 @@ const decksReducer = (state = {
     loading: false
 }, action) => {
     switch(action.type){
+        case "LOADING_DECKS":
+            return {
+                ...state,
+                decks: [...state.decks],
+                loading: true
+            }
+
+        case "ADD_CATS":
+            return {
+                ...state,
+                loading: false
+            }
+            
         default:
             return state
     }
