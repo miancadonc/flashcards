0269df1d7993d6fc7408aed78d970ad814625e49
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-29 20:16:24 -0500

    initial configure index.js to use redux and thunk

--- frontend/flashcards/src/index.js
+++ frontend/flashcards/src/index.js
@@ -4,18 +4,23 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
-const store = createStore()
+import decksReducer from './reducers/decksReducer'
+
+const store = createStore(decksReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 // reportWebVitals();
