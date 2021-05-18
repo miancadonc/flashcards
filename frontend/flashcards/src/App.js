e876f7bbfae2a89c229a9b4fca685d92ee0bc899
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-05-18 12:47:21 -0400

    add individual deck routes to app

--- frontend/flashcards/src/App.js
+++ frontend/flashcards/src/App.js
@@ -3,7 +3,7 @@
 
 import { Component } from "react";
 import { connect } from "react-redux";
-import {fetchDecks, addDeck} from './actions/deckActions'
+import {fetchDecks, addDeck, addCard} from './actions/deckActions'
 import Deck from "./components/Deck";
 import Flashcard from "./components/Flashcard";
 
@@ -11,6 +11,8 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import NavBar from './components/NavBar'
 import DecksPage from './containers/DecksPage'
 import DeckInput from './components/DeckInput'
+import CardInput from './components/CardInput'
+import CardsPage from "./containers/CardsPage";
 
 // function App() {
 //   return (
@@ -45,8 +47,7 @@ class App extends Component {
     if(this.props.loading === false && !!this.props.decks[0]){
       return(
       <div>
-        {/* <Deck deck={this.props.decks[0]}/>
-        <Flashcard flashcard={this.props.decks[0].cards[0]} /> */}
+        {/* <Flashcard flashcard={this.props.decks[0].cards[0]} /> */}
         <DecksPage decks={this.props.decks} />
         
       </div>
@@ -61,8 +62,12 @@ class App extends Component {
         <div>
         <NavBar/>
         <Route exact path="/" render={() => <div>{this.handleLoading()}</div>} />
-        <Route path="/decks" render={() => <div>{this.handleLoading()}</div>} />
+        <Route exact path="/decks" render={() => <div>{this.handleLoading()}</div>} />
         <Route path="/decks/new" render={() => <DeckInput addDeck={this.props.addDeck} />} />
+        <Route path="/decks/:id/cards" render={(routerProps) => <CardsPage {...routerProps} decks={this.props.decks} />} />
+        {/* <Route path="/decks/:id/cards" component={<CardsPage decks={this.props.decks} />} /> */}
+        <Route path="/decks/:id/cards/new" render={(routerProps) => <CardInput {...routerProps} addCard={this.props.addCard} />} />
+        {/* <Route path="/cards/new" render={() => <CardInput addCard={this.props.addCard} />} /> */}
         </div>
       </Router>
     )
@@ -76,7 +81,10 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   fetchDecks: () => dispatch(fetchDecks()),
-  addDeck: (deck) => dispatch(addDeck(deck))
+  addDeck: (deck) => dispatch(addDeck(deck)),
+  addCard: (card) => dispatch(addCard(card))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+
